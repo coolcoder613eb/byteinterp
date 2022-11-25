b135c24e061b038fabedb1ca7d9570b50e27ebf3
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2022-11-25 11:33:58 +1100

    update

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace byteinterp
     class Program
     {
         const byte outcode = 1;
+        const byte outseq = 2;
+        const byte endseq = 3;
         static void Main(string[] args)
         {
             byte[] bytes = File.ReadAllBytes(args[0]);
@@ -26,6 +28,20 @@ namespace byteinterp
                     {
                         case outcode:
                             Console.Write((Convert.ToChar(code.Dequeue())).ToString());
+                            break;
+                        case outseq:
+                            while (true)
+                            {
+                                byte ccode = code.Dequeue();
+                                if (ccode == endseq)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.Write((Convert.ToChar(ccode)).ToString());
+                                }
+                            }
                             break;
 
                     }
