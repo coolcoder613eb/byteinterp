9d8ba7bc4990d22148a555ebc1dc7410274978ed
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2022-12-04 11:37:21 +1100

    update

--- easm.py
+++ easm.py
@@ -1,9 +1,18 @@
-import os
+# import os
 import sys
 import shlex
 
+
 class EasmError(Exception):
     pass
+
+
+if '-d' in sys.argv or '--debug' in sys.argv:
+    debug = True
+    if sys.argv[1] == '-d' or sys.argv[1] == '--debug':
+        sys.argv.pop(1)
+else:
+    debug = False
 
 try:
     with open(sys.argv[1], 'r', encoding='utf-8') as f:
@@ -11,11 +20,6 @@ try:
     interactive = False
 except:
     interactive = True
-
-if '-d' in sys.argv or '--debug' in sys.argv:
-    debug = True
-else:
-    debug = False
 
 exitprog = sys.exit
 
@@ -25,7 +29,7 @@ def pushint():
     if statement and type(statement) == int:
         int_stack.append(statement)
     else:
-        raise EasmError('Error!')
+        raiseerror('Error in pushint!')
     return None
 
 
@@ -34,7 +38,7 @@ def pushstr():
     if statement and type(statement) == str:
         str_stack.append(statement)
     else:
-        raise EasmError('Error!')
+        raiseerror('Error in pushstr!')
     return None
 
 
@@ -46,8 +50,16 @@ def pullstr():
     return str_stack.pop()
 
 
+def peekint():
+    return int_stack[-1]
+
+
+def peekstr():
+    return str_stack[-1]
+
+
 def string():
-    return str(int_stack.pop())
+    return str(evaleasm())
 
 
 def toint():
@@ -66,28 +78,59 @@ def div():
     return str(int_stack[-1] / int_stack[-2])
 
 
+def concat():
+    return str_stack[-2] + str_stack[-1]
+
+
+def strvar():
+    var_name = evaleasm(isname=True)
+    statement = evaleasm()
+    if statement and type(statement) == str:
+        str_vars.update({var_name: statement})
+    else:
+        raiseerror('Error in strvar!')
+
+    return None
+
+
+def intvar():
+    var_name = evaleasm(isname=True)
+    statement = evaleasm()
+    if statement and type(statement) == int:
+        int_vars.update({var_name: statement})
+    else:
+        raiseerror('Error in intvar!')
+
+    return None
+
+
 def show():
     statement = evaleasm()
+    #print([statement], [type(statement)])
     if statement and type(statement) == str:
         if interactive:
             print(statement)
         else:
             print(statement, end='')
     else:
-        raise EasmError('Error!')
+        raiseerror('Error in show!')
     # print('n')
 
     return None
 
 
 proglines = []
-coms = {'pushint': pushint, 'pushstr': pushstr, 'pullint': pullint, 'pullstr': pullstr, 'string': string, 'int': toint,
-        'show': show, 'add': add, 'mult':mult , 'div': div, 'exit': exitprog}
+coms = {'pushint': pushint, 'pushstr': pushstr, 'pullint': pullint, 'pullstr': pullstr, 'peekint': peekint,
+        'peekstr': peekstr, 'string': string, 'int': toint, 'concat': concat,
+        'show': show, 'add': add, 'mult': mult, 'div': div, 'exit': exitprog,
+        'intvar': intvar, 'strvar': strvar}
 
 # coms = ['pushint', 'pushstr', 'pullint', 'pullstr', 'string', 'int', 'show']
 
 str_stack = []
 int_stack = []
+str_vars = {}
+int_vars = {}
 
 
 def tonum(num):
@@ -115,19 +158,49 @@ def iscom(com):
         return False
 
 
+def isintvar(statement):
+    if statement in int_vars.keys():
+        return True
+    else:
+        return False
+
+
+def isstrvar(statement):
+    if statement in str_vars.keys():
+        return True
+    else:
+        return False
+
+
 # print('\n'.join(proglines))
 
+def raiseerror(err):
+    sys.stderr.write('Error: ' + err)
+    sys.exit()
 
-def evaleasm():
+
+def evaleasm(isname=False):
     statement = prog[r].pop(0)
+    if isname:
+        return statement
     isstr = tostr(statement)
     isnum = tonum(statement)
+    is_com = iscom(statement)
+    is_strvar = isstrvar(statement)
+    is_intvar = isintvar(statement)
+
     if debug:
         # print('statement:',statement,'| is string:', [isstr],'| is num:', [isnum],'| int stack:', int_stack,'| str stack:', str_stack)
-        print('statement:', [statement], 'is string:', [isstr], 'is num:', [isnum], 'int stack:', int_stack,
-              'str stack:', str_stack)
-    if iscom(statement):
+        print('statement:', [statement], 'is command:', [is_com], 'is string:', [isstr], 'is num:', [isnum],
+              'is str var:', [is_strvar], 'is int var:', [is_intvar], 'int stack:', int_stack,
+              'str stack:', str_stack, 'str vars:', [str_vars], 'int vars:', [int_vars])
+    if is_com:
         return coms[statement]()
+    if is_strvar:
+        #print(str_vars, statement)
+        return str_vars[statement]
+    if is_intvar:
+        return int_vars[statement]
     if isnum:
         return isnum
     if isstr:
